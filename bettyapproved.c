5bb6a0c6e888f9f77c890d5371db706aaa9acd5f
Keny09 <rosekendy09@gmail.com>
2023-05-22 02:23:45 -0700

    simpleshell

--- bettyapproved.c
+++ bettyapproved.c
@@ -1,26 +1,28 @@
-#include "main.h"
+nclude "main.h"
 /**
- * cube_function: shows the cube of a number
+ * cube_function - shows the cube of a number
  * @num: The number to be cubed
- * return: cube function
+ * Return: cube function
  */
 int cube_function(int num)
 {
-int cube = num * num * num;
-return (cube);
+	int cube;
+
+	cube = num * num * num
+	return (cube);
 }
 /**
- * main: Entry point of the program
- * return: 0 (success)
+ * main - Entry point of the program
+ * Return: 0 (success)
  */
 int main(void)
 {
-int number;
-int solution;
+	int number;
+	int solution;
 
-printf("Enter number: ");
-scanf("%d", number);
-solution = cube_function(number);
-printf("The cube of %d is %d\n", number, solution);
-return (0);
+	printf("Enter number: ");
+	scanf("%d", number);
+	solution = cube_function(number);
+	printf("The cube of %d is %d\n", number, solution);
+	return (0);
 }
