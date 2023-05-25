b6b0ff64852e2116d6cd542f2022f1f915f23de5
ochiengsulwe <kodongochieng@gmail.com>
2023-05-25 06:37:11 -0700

    routine cleanup

--- tests/gcc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "MAINFILE"
-read MAIN
-echo "CFILE"
-read CFILE
-echo "EXEC"
-read EXEC
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 $MAIN $CFILE -o $EXEC
--- tests/pid_max.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#prints the maximum value a process ID can be
-
-max_pid=$(cat /proc/sys/kernel/pid_max)
-echo $max_pid
